a577ae55a7d33d5b3c91b2f274c7813ad380d4d8
tablonimus <lautamartin1995@gmail.com>
2023-02-01 18:29:49 -0300

    ejercicio 10

---
+++ Tarea 1/index.js
@@ -0,0 +1,40 @@
+// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = null;
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = null;
+
+// Crea una variable booleana:
+const nuevoBooleano = null;
+
+//Resuelve el siguiente problema matemático para que el resultado sea igual a 15:
+const nuevaResta = null - 20;
+
+// Resuelve el siguiente problema matemático para que el resultado sea igual a 358//
+const nuevaMultiplicacion = 2 * null;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % null === 1;
+
+// Resuelve el siguiente problema matemático:
+const mayorQue50 = null > 50;
+
+// Resuelve el siguiente problema matemático, reemplazando null por la ecuación correspondiente:
+var base = null;
+var altura = null;
+const areaDeUnCuadrado = null; /* Aqui va una formula matematica */
+
+// Resuelve segun los operadores logicos lo indiquen ( || , &&)
+
+//Reemplaza los valores en las variables numero* para obtener el resultado deseado.
+var numero1 = 1;
+var numero2 = 1;
+
+const esMultiploDe5 = numero1 % 5 === 0 || numero2 % 5 === 0;
+
+//
+var numero3 = null;
+var numero4 = null;
+const esMayorQue20 = numero3 && numero4;
