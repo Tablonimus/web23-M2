c979e6bc91555a8c200b61de055c376fbe45a1d0
tablonimus <lautamartin1995@gmail.com>
2023-02-08 17:40:05 -0300

    ahora

--- Tarea 1/index.js
+++ Tarea 1/index.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
-
+console.log("escribi algo")
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "why";//almacenarlo en un espacio en la memoria
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
@@ -10,7 +10,7 @@ const nuevoNum = null;
 const nuevoBooleano = null;
 
 //Resuelve el siguiente problema matemático para que el resultado sea igual a 15:
-const nuevaResta = null - 20;
+const nuevaResta = 35 - 20;
 
 // Resuelve el siguiente problema matemático para que el resultado sea igual a 358//
 const nuevaMultiplicacion = 2 * null;
@@ -19,22 +19,22 @@ const nuevaMultiplicacion = 2 * null;
 const nuevoModulo = 21 % null === 1;
 
 // Resuelve el siguiente problema matemático:
-const mayorQue50 = null > 50;
+const mayorQue50 = true; 
 
 // Resuelve el siguiente problema matemático, reemplazando null por la ecuación correspondiente:
-var base = null;
-var altura = null;
-const areaDeUnCuadrado = null; /* Aqui va una formula matematica */
+var base = 25;
+var altura = 25;
+const areaDeUnCuadrado = base*altura; /* Aqui va una formula matematica */
 
-// Resuelve segun los operadores logicos lo indiquen ( || , &&)
+// Resuelve segun los operadores logicos lo indiquen ( || or ó , &&)
 
 //Reemplaza los valores en las variables numero* para obtener el resultado deseado.
 var numero1 = 1;
-var numero2 = 1;
+var numero2 = 15;
 
 const esMultiploDe5 = numero1 % 5 === 0 || numero2 % 5 === 0;
 
 //
-var numero3 = null;
-var numero4 = null;
+var numero3 = 24;
+var numero4 = 21;
 const esMayorQue20 = numero3 && numero4;
